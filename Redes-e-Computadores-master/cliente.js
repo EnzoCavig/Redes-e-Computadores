fe79c97eca10743da62c1c964b7c7d04a7af5d44
Enzo Caviglia <88462886+EnzoCavig@users.noreply.github.com>
2022-03-18 21:53:21 -0300

    Add files via upload

--- Redes-e-Computadores-master/cliente.js
+++ Redes-e-Computadores-master/cliente.js
@@ -1,43 +1,68 @@
 
-    var sNomeDivCliente = 'servidor';
+  
+    
+var sNomeDivCliente = 'cliente';
 
-    function onClickBotaoCliente() {
-        debugger;
-        setTimeout(chamaRequisicaoServidor, 2000);
-        setTimeout(aguardandoRespostaServidor, 3000);
-        setTimeout(respostaRequisicaoServidor, 5000);
-    };
-
-    function chamaRequisicaoServidor(){
-        let oDivCliente = document.getElementById(sNomeDivCliente);
-        oDivCliente.innerHTML = getRequisaoServidor();    
+function onClickBotaoCliente() {
+    debugger;
+    setTimeout(chamaRequisicaoServidor, 2000);
+    setTimeout(aguardandoRespostaServidor, 3000);
+    debugger;
+    if(document.getElementById('lista_servidor').value == 'ativado'){
+        setTimeout(respostaRequisicaoServidorOk, 5000);
+    } else {
+        setTimeout(respostaRequisicaoServidorFail, 5000);
     }
+    
+};
 
-    function aguardandoRespostaServidor(){
-        let oDivCliente = document.getElementById(sNomeDivCliente);
-        oDivCliente.innerHTML = oDivCliente.innerHTML + "<br> Aguardando resposta do Servidor:<br>"
-    }
+function chamaRequisicaoServidor(){
+    let oDivCliente = document.getElementById(sNomeDivCliente);
+    oDivCliente.innerHTML = getRespostaServidorOk();    
+}
 
-    function respostaRequisicaoServidor() {
-        let oDivCliente = document.getElementById(sNomeDivCliente);
-        oDivCliente.innerHTML = oDivCliente.innerHTML + '<br>' + getRespostaServidor();
-    }
+function aguardandoRespostaServidor(){
+    let oDivCliente = document.getElementById(sNomeDivCliente);
+    oDivCliente.innerHTML = oDivCliente.innerHTML + "<br> Aguardando resposta do Servidor:<br>"
+}
 
-    function getRequisaoServidor(){
-        let sLink = document.getElementById('lista').value;
-        return 'Get /'+ sLink + '<br>'
-              + 'Accept-Language:  pt-br<br>'
-              + 'Accept:<br>'  
-              + 'Connection: close<br>'
-              + 'Host: 192.168.1.100<br>';
-    };
+function respostaRequisicaoServidorOk() {
+    let oDivCliente = document.getElementById(sNomeDivCliente);
+    oDivCliente.innerHTML = oDivCliente.innerHTML + '<br>' + getRespostaServidor();
+}
 
-    function getRespostaServidor() {
-        let sLink = document.getElementById('lista').value;
-        return sLink + ' 200 OK <br>'
-             + 'Connection: close <br>'
-             + 'Content-Length: 357 <br>'
-             + 'Content-Type: text/html <br>'
-             + 'Server: PT-Server/5.2 <br>'
-             + "HTTP DATA..' <br>";
-    }
+function respostaRequisicaoServidorFail() {
+    let oDivCliente = document.getElementById(sNomeDivCliente);
+    oDivCliente.innerHTML = oDivCliente.innerHTML + '<br>' + getRespostaServidorFail();
+}
+
+function getRequisaoServidor(){
+    let sLink = document.getElementById('lista').value;
+    return 'Get /'+ sLink + '<br>'
+          + 'Accept-Language:  pt-br<br>'
+          + 'Accept:<br>'  
+          + 'Connection: close<br>'
+          + 'Host: 192.168.1.100<br>';
+};
+
+function getRespostaServidorOk() {
+    let sLink = document.getElementById('lista').value;
+    return sLink + ' 200 OK <br>'
+         + 'Connection: close <br>'
+         + 'Content-Length: 357 <br>'
+         + 'Content-Type: text/html <br>'
+         + 'Server: PT-Server/5.2 <br>'
+         + "HTTP DATA..' <br>";
+}
+
+function getRespostaServidorFail() {
+    let date = new Date();
+    let sLink = document.getElementById('lista').value;
+    return sLink + ' 502 BAD GATEWAY <br>'
+         + 'date: ' + date + ' <br>'
+         + 'content-type: text/html; charset=UTF-8 <br>'
+         + 'transfer-encoding: chunked <br>'
+         + 'connection: close <br>'
+         + 'server:  PT-Server/5.2<br>'
+         + 'content-encoding: gzip <br>'
+}
